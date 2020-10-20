1a67376a91d24e15aef465819d366bec22d70b55
book_swag <its.bookswag@gmail.com>
2020-10-20 20:24:27 +0900

    fix to follow go-lint

--- hola.go
+++ hola.go
@@ -5,7 +5,7 @@ import "fmt"
 func hola(name string) string {
 	if len(name) == 0 {
 		return "Hola Dude!"
-	} else {
-		return fmt.Sprintf("Hola %v!", name)
 	}
+
+	return fmt.Sprintf("Hola %v!", name)
 }
