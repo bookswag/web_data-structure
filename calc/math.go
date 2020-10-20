2243c566d5ed5fbf433ec5a44a196c4c1886fb74
book_swag <its.bookswag@gmail.com>
2020-10-20 20:32:43 +0900

    add test sample : math with error

---
+++ calc/math.go
@@ -0,0 +1,17 @@
+package calc
+
+import "errors"
+
+func Add(numbers ...int) (error, int) {
+	sum := 0
+
+	if len(numbers) < 2 {
+		return errors.New("provide more than 2 numbers"), sum
+	}
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return nil, sum
+}
